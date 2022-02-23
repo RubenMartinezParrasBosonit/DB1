decf20558f6ccba7139842049e573e296a4e9cef
Rubén Martínez Parras <ruben.mparras@bosonit.com>
2022-02-23 16:15:58 +0100

    Ejercicio BS9 completado

--- src/main/java/io/github/ruben/profesor/infrastructure/controller/ReadProfesorController.java
+++ src/main/java/io/github/ruben/profesor/infrastructure/controller/ReadProfesorController.java
@@ -3,17 +3,23 @@ package io.github.ruben.profesor.infrastructure.controller;
 import io.github.ruben.profesor.application.ProfesorService;
 import io.github.ruben.profesor.infrastructure.controller.dto.output.ProfesorOutputDto;
 import io.github.ruben.profesor.infrastructure.controller.dto.output.ProfesorOutputDtoList;
+import io.github.ruben.profesor.infrastructure.feing.IFeingProfesor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RequestMapping("profesor")
 @RestController
 public class ReadProfesorController {
     @Autowired
     ProfesorService profesorService;
+
+    @Autowired
+    IFeingProfesor iFeingProfesor;
 
     @GetMapping
     public ProfesorOutputDtoList findAll(){
@@ -23,5 +29,21 @@ public class ReadProfesorController {
     @GetMapping("{id}")
     public ProfesorOutputDto getProfesorById(@PathVariable String id){
         return profesorService.filtrarProfesorPorId(id);
+    }
+
+    @GetMapping("/template/{id}")
+    ResponseEntity<ProfesorOutputDto> getProfesorRestTemplate(@PathVariable String id){
+        System.out.println("En client Resttemplate. Antes de llamada a server Profesor: "+id);
+        ResponseEntity<ProfesorOutputDto> rs = new RestTemplate().getForEntity("http://localhost:8081/profesor/"+id,ProfesorOutputDto.class);
+        System.out.println("En client Resttemplate. Despues de llamada a server");
+        return ResponseEntity.ok(rs.getBody());
+    }
+
+    @GetMapping("/feing/{id}")
+    ResponseEntity<ProfesorOutputDto> getProfesorFeing(@PathVariable String id){
+        System.out.println("En client Feing. Antes de llamada a server Profesor: "+id);
+        ResponseEntity<ProfesorOutputDto> rsFeing = iFeingProfesor.callProfesor(id);
+        System.out.println("En client Feing. Despues de llamada a server");
+        return rsFeing;
     }
 }
---
+++ src/main/java/io/github/ruben/profesor/infrastructure/feing/IFeingProfesor.java
@@ -0,0 +1,14 @@
+package io.github.ruben.profesor.infrastructure.feing;
+
+import io.github.ruben.profesor.infrastructure.controller.dto.output.ProfesorOutputDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="simpleFeign",url="http://localhost:8081/")
+public interface IFeingProfesor {
+
+    @GetMapping("/profesor/{id}")
+    ResponseEntity<ProfesorOutputDto> callProfesor(@PathVariable("id") String id);
+}
